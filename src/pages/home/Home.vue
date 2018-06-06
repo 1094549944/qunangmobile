6121d772bac3fa5466b283d55ab6a01a7e50c58e
jiaxinying <jiaxinying@91taogu.com>
2018-06-06 20:13:44 +0800

    jia 首页

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <home-header :city="city"></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <weekend :list="weekendList"></weekend>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/header'
+import HomeSwiper from './Swiper.vue'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import Weekend from './components/Weekend'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    Weekend
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  }
+}
+</script>
---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#58930;</span>输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      {{this.city}}
+      <span class="iconfont arrow-icon">&#58954;</span>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'HomeHeader',
+  props: {
+    city: String
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles'
+
+.header
+  line-height 0.86rem
+  display flex
+  background $bgColor
+  color #fff
+
+  .header-left
+    width 0.64rem
+    float left
+
+    .back-icon
+      text-align center
+      font-size 0.4rem
+
+  .header-input
+    flex 1
+    height 0.64rem
+    line-height 0.64rem
+    margin-top 0.12rem
+    margin-left 0.2rem
+    padding-left 0.2rem
+    background #fff
+    border-radius 0.1rem
+    color #ccc
+
+    .iconfont
+      padding-right 0.2rem
+
+  .header-right
+    min-width 1.04rem
+    padding 0 0.1rem
+    float right
+    text-align center
+    color #fff
+
+    .arrow-icon
+      margin-left -0.04rem
+</style>
